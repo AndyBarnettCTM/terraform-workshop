b34e18ecd81cb129f76315fcd9ceba329af1e863
AndyBarnettCTM <andrew.barnett@bglgroup.co.uk>
2019-09-05 00:01:52 +0100

    added andys tg

--- target-groups.tf
+++ target-groups.tf
@@ -19,3 +19,25 @@ resource "aws_alb_target_group" "workshop_tg" {
     service = "workshop"
   }
 }
+
+resource "aws_alb_target_group" "andys_target_group" {
+  name     = "andys-tg"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = "vpc-04836362"
+
+  health_check {
+    interval            = var.health_check_interval
+    port                = "443"
+    protocol            = "HTTP"
+    timeout             = var.health_check_timeout
+    healthy_threshold   = "2"
+    unhealthy_threshold = "2"
+    matcher             = "200"
+  }
+
+  tags = {
+    Name    = "workshop"
+    service = "workshop"
+  }
+}
